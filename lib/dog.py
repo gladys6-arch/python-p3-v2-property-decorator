5dfc078f42b7583f96f7a2bb4a2d76ce27f50981
gladys <achandogladys6@gmail.com>
2025-08-10 23:42:47 +0300

    add new changes

--- lib/dog.py
+++ lib/dog.py
@@ -15,9 +15,12 @@ class Dog:
         self.name = name
         self.breed = breed
 
+
     def get_name(self):
         return self._name
+    
 
+    
     def set_name(self, name):
         if isinstance(name, str) and 1 <= len(name) <= 25:
             self._name = name.title()
@@ -27,9 +30,12 @@ class Dog:
 
     name = property(get_name, set_name)
 
+
     def get_breed(self):
         return self._breed
+    
 
+    
     def set_breed(self, breed):
         if breed in APPROVED_BREEDS:
             self._breed = breed
